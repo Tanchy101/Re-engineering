981025586b02f00a8b21b9e7047ab0468b02479d
JamesSama <cj31defita@gmail.com>
2023-11-18 22:59:47 +0800

    testpush
    sa ikapapanatag ng lahat.

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => '__root__',
-        'pretty_version' => 'dev-master',
-        'version' => 'dev-master',
-        'reference' => 'd492285674c5fa10e3ec5e80dff04234abfc275a',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '1020edc399759ea8c8ac7bdfd650febac13983f2',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         '__root__' => array(
-            'pretty_version' => 'dev-master',
-            'version' => 'dev-master',
-            'reference' => 'd492285674c5fa10e3ec5e80dff04234abfc275a',
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '1020edc399759ea8c8ac7bdfd650febac13983f2',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
