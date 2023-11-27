bef4c3dc256ed36adccf6f7f747d5ab22ec763d5
Jenie O. Tadena <102201926+Jeniebean@users.noreply.github.com>
2023-11-27 18:12:43 +0800

    Admin Index
    Gumawa ako ng Admin Index, wherein nag create ako ng layout through HTML with CSS  para sa mga pending, to pack, and transit panel.

---
+++ admin/adminIndex.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin View</title>
+    <style>
+        body {
+            font-family: Montserrat;
+            margin: 0;
+            padding: 10;
+            background-color: #36517C;
+            box-sizing: border-box;
+        }
+
+        header {
+            background-color: #36517C;
+            color: #fff;
+            padding: 0px;
+            margin-bottom: 50px;
+            text-align: left;
+            align-items: left; margin-left: 290px;
+      
+        }
+
+        nav {
+            background-color: #5DCAD1;
+            padding: 10px;
+            width: 70%; 
+            margin: auto; 
+            background-color: rgb(213, 211, 209);
+            align-items: left; margin-left: 360px;
+            border-radius: 10px;
+            flex-content: row;
+        }
+
+        nav ul {
+            list-style-type: none;
+            margin: 0;
+            padding: 2;
+            overflow: hidden;
+        }
+
+        nav li {
+            display: inline;
+            margin-right: 120px;
+        }
+        
+       nav a {
+            text-decoration: none;
+            color: #000000;
+        }
+
+       .box {
+            width: 1090px; /* Set the width as needed */
+            height: 500px; /* Set the height as needed */
+            background-color: #FFFFFF; /* Set the background color as needed */
+            border: 1px solid #ccc; /* Set the border as needed */
+            padding: 0px; /* Set the padding as needed */
+            margin: 20px; /* Set the margin as needed */
+            align-items: left; margin-left: 360px;
+        }
+
+    </style>
+</head>
+<body>
+
+    <header>
+        <h1>ADMIN VIEW</h1>
+        <div class="left-align">
+      </div>
+    </header>
+
+
+    <nav>
+        <ul>
+            <li>USER&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp</li>
+            <li>ADDRESS&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp</li>
+            <li>PRODUCT&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp</li>
+            <li>PRICE QUANTITY AMOUNT</li>
+            <div class="left-align">
+          </div>
+        </ul>
+    </nav>
+
+      <div class="box">
+       </div>
+     <div class="left-align">
+       </div>
+ 
+
+</body>
+</html> 
