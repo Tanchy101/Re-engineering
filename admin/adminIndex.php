7bbcb0a1363b072ec66b1c4702814c5e96d05e44
Jenie O. Tadena <102201926+Jeniebean@users.noreply.github.com>
2023-12-01 01:49:25 +0800

    Admin Sidebar
    nilagyan ko na ng sidebar yung adminIndex then naayos nadin yung tabs plus yung lagayan ng data kung saan maiinput ang mga data sa admin panel

--- admin/adminIndex.php
+++ admin/adminIndex.php
@@ -15,37 +15,127 @@ include('../config/config.php');
 </head>
 <body>
 
+  <!-- Sidenav -->
+  <?php
+  require_once('../partials/_adminsidebar.php');
+  ?>
+
+ 
 <header>
   <h1>ADMIN VIEW</h1>
 </header>
 
+
 <div class="mytabs">
   <input type="radio" id="tabpending" name="mytabs" checked="checked">
-  <label for="tabpending" style="background-color: #99948F;">PENDING</label>
+  <label for="tabpending" style="background-color: #99948F;"><h5>PENDING</h5></label>
   <div class="tab">
-      <div class="styled-box" style="background-color: #99948F;">
-        <h2 style="margin-top: -0.2.5em; margin-left: 0.4em;">USER&nbsp;&nbsp;ADDRESS&nbsp;&nbsp;PRODUCT&nbsp;&nbsp;PRICE QUANTITY AMOUNT</h2>
+      <div class="styled-box" style="background-color: rgba(213, 211, 209, 8)">
+      <div style="overflow-x: auto; text-align: left;">
+          <table>
+            <tr>
+              <th>USER</th> 
+              <th>ADDRESS</th> 
+              <th>PRODUCT</th> 
+              <th>PRICE</th> 
+              <th>QUANTITY</th> 
+              <th>AMOUNT</th>
+            </tr>
+            <tr>
+              <td>hev jenie asdasdasd</td>
+              <td>1157 marzan st. sampaloc, manila</td>
+              <td>kush</td>
+              <td>300php</td>
+              <td>2 grams</td>
+              <td>600php</td>
+            </tr>  
+            <tr>
+              <td>hev jenie asdasdasd</td>
+              <td>1157 marzan st. sampaloc, manila</td>
+              <td>kush</td>
+              <td>300php</td>
+              <td>2 grams</td>
+              <td>600php</td>
+            </tr>  
+          </table>
+        </div>
       </div>
-      <p>User1                  Address                   Product                 Price Quantity Amount</p>
     </div>
+
+
 
     <input type="radio" id="tabtopack" name="mytabs">
-    <label for="tabtopack" style="background-color: #FAB6AB;">TO PACK</label>
+    <label for="tabtopack" style="background-color: #FAB6AB;"><h5>TO PACK</h5></label>
     <div class="tab">
-      <div class="styled-box" style="background-color: #FAB6AB;"></div>
-      <h2 style="margin-top: -1.9em; margin-left: -0.1em;">USER&nbsp;&nbsp;ADDRESS&nbsp;&nbsp;PRODUCT&nbsp;&nbsp;PRICE QUANTITY AMOUNT</h2>
-      <p>User1                  Address                   Product                 Price Quantity Amount</p>
+      <div class="styled-box" style="background-color: rgba(252, 225, 217, 8);">
+        <div style="overflow-x: auto; text-align: left;">
+          <table>
+            <tr>
+              <th>USER</th> 
+              <th>ADDRESS</th> 
+              <th>PRODUCT</th> 
+              <th>PRICE</th> 
+              <th>QUANTITY</th> 
+              <th>AMOUNT</th>
+            </tr>
+            <tr>
+              <td>hev jenie asdasdasd</td>
+              <td>1157 marzan st. sampaloc, manila</td>
+              <td>kush</td>
+              <td>300php</td>
+              <td>2 grams</td>
+              <td>600php</td>
+            </tr>  
+            <tr>
+              <td>hev jenie asdasdasd</td>
+              <td>1157 marzan st. sampaloc, manila</td>
+              <td>kush</td>
+              <td>300php</td>
+              <td>2 grams</td>
+              <td>600php</td>
+            </tr>  
+          </table>
+        </div>
+      </div>
     </div>
+
 
     <input type="radio" id="tabintransit" name="mytabs">
-    <label for="tabintransit" style="background-color: #5DCAD1;">IN TRANSIT</label>
+    <label for="tabintransit" style="background-color: #5DCAD1;"><h5>IN TRANSIT</h5></label>
     <div class="tab">
-      <div class="styled-box" style="background-color: #5DCAD1;"></div>
-      <h2 style="margin-top: -1.9em; margin-left: -0.1em;">USER&nbsp;&nbsp;ADDRESS&nbsp;&nbsp;PRODUCT&nbsp;&nbsp;PRICE QUANTITY AMOUNT</h2>
-      <p>User1                  Address                   Product                 Price Quantity Amount</p>
-    </div>
+      <div class="styled-box" style="background-color: rgba(189, 233, 232, 8);">
+      <div style="overflow-x: auto; text-align: left;">
+          <table>
+            <tr>
+              <th>USER</th> 
+              <th>ADDRESS</th> 
+              <th>PRODUCT</th> 
+              <th>PRICE</th> 
+              <th>QUANTITY</th> 
+              <th>AMOUNT</th>
+            </tr>
+            <tr>
+              <td>hev jenie asdasdasd</td>
+              <td>1157 marzan st. sampaloc, manila</td>
+              <td>kush</td>
+              <td>300php</td>
+              <td>2 grams</td>
+              <td>600php</td>
+            </tr>  
+            <tr>
+              <td>hev jenie asdasdasd</td>
+              <td>1157 marzan st. sampaloc, manila</td>
+              <td>kush</td>
+              <td>300php</td>
+              <td>2 grams</td>
+              <td>600php</td>
+            </tr>  
+          </table>
+        </div>
+      </div>
 
   </div>
+
 </body>
 </html>
 
@@ -72,32 +162,48 @@ include('../config/config.php');
 
  .styled-box { 
   background-color: #99948F; 
-  padding: 12px;
-  width: 66.65em;
-  height: 1em;
+  padding: auto;
+  width: 65.60em;
+  height: 2.5em;
   margin: 0px; 
   border-radius: 5px; 
   align-items: left; margin-left: -1.25em;
   margin-top:   -1.25em;
   margin-right: 0.1em;
   border-radius: 0px;
-   
 }
 
 .top-text {
       margin-top: 20px; /* Adjust the margin-top value as needed */
     }
 
-h2 {
-  font-size: 16px;
-  margin-top: -0.15em;
-  margin-left: 0.15px;
-
+th {
+  font-size: 14px;
+  font-weight: lighter;
+  top: 2em;
+  padding-bottom: 18px;
+  padding-top: 10px;
+  
   
 }
 
+th,td{
+  padding-left: 20px;
+  column-gap: 40px;
+}
+
+td{
+  line-height: 1.0;
+  
+}
+
+h5{
+  font-size: 15px;
+  margin-top: -0.3em;
+}
+
 p {
-  margin-top: 2.02px;
+  margin-top: 6.20px;
   padding: 12px;
   margin-left: -0.85em;
 }
@@ -117,15 +223,15 @@ h4 {
 } 
 
 .mytabs {
-    position: fixed;
     display: flex;
     flex-wrap: wrap;
-    max-width: 1050px;
+    max-width: 1100px;
     padding: 25px;
-    align-items: left; margin-left: 360px;
+    align-items: left; margin-left: 335px;
     border-radius: 10px;
     flex-content: row;
     height: 1em;
+    position: fixed;
     
     
 }
@@ -134,15 +240,18 @@ h4 {
 }
 
 .mytabs label {
-    padding: 20px;
+    padding: 23px;
     background: #e2e2e2;
     font-weight: bold;
     border-top-right-radius: 20px;
     border-top-left-radius: 20px;
-    height: 10px;
-    width: 10em;
+    height: 13px;
+    width: 13em;
     cursor: pointer;
     text-align: center;
+    margin-top: 1em;
+    flex-content: center;
+    
     
 }
 
@@ -165,7 +274,5 @@ h4 {
 .mytabs input[type="radio"]:checked + label {
     background: #99948F;
 }
-
-
 
 </style>
