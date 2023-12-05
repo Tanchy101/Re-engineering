734915448b0f3edcc5451d5685380934914ae939
JamesSama <cj31defita@gmail.com>
2023-12-06 00:05:24 +0800

    makeplantest3bgupdate

--- makeplantest3.php
+++ makeplantest3.php
@@ -121,8 +121,7 @@ function captureAndPrint() {
     require_once('partials/_topnav.php');
     ?>
     <!-- Header -->
-    <div style="background-image: url(assets/img/theme/restro00.jpg); background-size: cover;" class="header  pb-8 pt-5 pt-md-8">
-    <span class="mask bg-gradient-dark opacity-8"></span>
+    <div style="background-image: url(assets/img/theme/GenerateBG.png); background-size: cover; background-height: 50%;" class="header  pb-8 pt-5 pt-md-8">
       <div class="container-fluid">
         <div class="header-body">
         </div>
